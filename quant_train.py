0e4124a39766ab8484e8de2940d5db24889e6c7b
Maryamsana-1998 <38385864+Maryamsana-1998@users.noreply.github.com>
2020-01-20 09:33:48 -0800

    Update quant_train.py

--- quant_train.py
+++ quant_train.py
@@ -65,7 +65,7 @@ net=QuantLeNet()
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 #define optimier
-for epoch in range(2):  # loop over the dataset multiple times
+for epoch in range(50):  # loop over the dataset multiple times
 
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
